efde6335ee46d218fe9582c9f9cdec949921ff83
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-11-02 20:21:28 -0400

    Created basic app interaction with the dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toDrawable
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btRoll: Button
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setViews()
+        setListeners()
+
     }
+
+    private fun setViews() {
+
+        btRoll = findViewById(R.id.bt_roll)
+        diceImage = findViewById(R.id.iv_dice)
+
+    }
+
+    private fun setListeners() {
+
+        btRoll.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+
+        when (Random.nextInt(1, 7)) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+    }
+
 }
