39178d5c979df75116cd66b2ba784fa744db2b42
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-11-02 23:50:14 -0400

    Added view binding content

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,23 +5,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    // It does not need anymore views id reference when using View Binding
+    /*
     private lateinit var btRoll: Button
     private lateinit var diceImage: ImageView
     private lateinit var diceValue: TextView
+    */
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setViews()
+        binding = ActivityMainBinding.inflate(layoutInflater) // View Binding
+        setContentView(binding.root)
+
+        // setViews() -> We don't need while using View Binding
         setListeners()
 
     }
 
+    // It does not need anymore views id reference when using View Binding
+    /*
     private fun setViews() {
 
         btRoll = findViewById(R.id.bt_roll)
@@ -29,10 +39,11 @@ class MainActivity : AppCompatActivity() {
         diceValue = findViewById(R.id.tv_dice_value)
 
     }
+    */
 
     private fun setListeners() {
 
-        btRoll.setOnClickListener {
+        binding.btRoll.setOnClickListener {
             rollDice()
         }
 
@@ -43,17 +54,17 @@ class MainActivity : AppCompatActivity() {
         val randomNumber = Random.nextInt(1, 7)
 
         when (randomNumber) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+            1 -> binding.ivDice.setImageResource(R.drawable.dice_1)
+            2 -> binding.ivDice.setImageResource(R.drawable.dice_2)
+            3 -> binding.ivDice.setImageResource(R.drawable.dice_3)
+            4 -> binding.ivDice.setImageResource(R.drawable.dice_4)
+            5 -> binding.ivDice.setImageResource(R.drawable.dice_5)
+            6 -> binding.ivDice.setImageResource(R.drawable.dice_6)
         }
 
         val textResult = "$randomNumber!"
 
-        diceValue.text = textResult
+        binding.tvDiceValue.text = textResult
 
     }
 
